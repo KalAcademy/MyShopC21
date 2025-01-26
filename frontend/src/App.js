216c5257664b73edbef6b21eac0d90ebd5627193
Kal <info@kalacademy.org>
2025-01-26 11:06:32 -0800

    Screens and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
+import {Container} from 'react-bootstrap'
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
       <Header />
-      <h1>Welcome to my shop</h1>
+      <main>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
       <Footer />
     </>
   );
