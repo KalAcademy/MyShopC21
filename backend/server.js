ee4dd78b7632dd8639d30aef67ebd5c68c8fdb88
Kal <info@kalacademy.org>
2025-02-16 11:01:19 -0800

    error middleware and getting started with redux

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import productRoutes from './routes/productRoutes.js'
 import connectDB from './config/db.js'
+import errorHandler from './middleware/errorMiddleware.js'
 
 const app = express()
 dotenv.config()
@@ -9,4 +10,5 @@ connectDB()
 
 app.use('/api/products', productRoutes)
 
+app.use(errorHandler)
 app.listen(5000, console.log('Server is running on port 5000'))
